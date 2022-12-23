ee2f8530c68c16d69a20f3a1c2f8a607d7aa24a2
vantt <van.tran@sotatek.com>
2022-12-23 13:36:14 +0700

    temp

---
+++ GIS_Technolory/GIS_Technolory/Entities/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using NetTopologySuite.Geometries;
+
+namespace GIS_Technolory.Entities
+{
+    public class Circle
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { get; set; }
+        [MaxLength(250)]
+        [Required]
+        public string Name { get; set; }
+
+        [NotMapped]
+        public double Latitude { get => Location.Y; }
+        [NotMapped]
+        public double Longitude { get => Location.X; }
+
+        [Required]
+        public Point Location { get; set; }
+
+        public string PopupContent { get; set; }
+
+        [MaxLength(10)]
+        public string ColorCircle { get; set; }
+
+        public int WeightBorder { get; set; }
+
+        public float Opacity { get; set; }
+
+        public int Radius { get; set; }
+    }
+}
---
+++ GIS_Technolory/GIS_Technolory/Entities/Polygon.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GIS_Technolory.Entities
+{
+    public class Polygon
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { get; set; }
+        [MaxLength(250)]
+        [Required]
+        public string Name { get; set; }
+        public string PopupContent { get; set; }
+
+        [NotMapped]
+        public bool IsRectangle
+        {
+            get => Location.IsRectangle;
+        }
+
+        /// <summary>
+        ///  Returns the count of this 
+        /// </summary>
+        [NotMapped]
+        public int NumPoints
+        {
+            get => Location.NumPoints;
+        }
+
+        /// <summary>
+        /// Returns the perimeter of this
+        /// </summary>
+        [NotMapped]
+        public double Length
+        {
+            get => Location.Length;
+        }
+
+        /// <summary>
+        /// Returns the area of this 
+        /// </summary>
+        public double Area
+        {
+            get => Location.Area;
+        }
+
+        [MaxLength(10)]
+        public string Color { get; set; }
+
+        public int WeightBorder { get; set; }
+
+        public float Opacity { get; set; }
+
+        public NetTopologySuite.Geometries.Polygon Location { get; set; }
+    }
+}
